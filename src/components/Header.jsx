57068639ebf805f6788cdf7e70c9b377a04032b0
mukulrajpoot262610 <branda56235@gmail.com>
2025-02-16 10:48:58 +0530

    H

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,12 +12,12 @@ const Header = () => {
             <div className=' h-screen w-full flex-col flex justify-center items-center  '>
                 <h1 className='text-[#fe6925] font-thin text-9xl'>SemWise</h1>
                 <div className='text-[#efefef] text-3xl'>
-                    <SignedOut>
+                    {/* <SignedOut>
                         <SignInButton />
                     </SignedOut>
                     <SignedIn>
                         <UserButton />
-                    </SignedIn>
+                    </SignedIn> */}
                 </div>
             </div>
         </>
